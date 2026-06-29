25e7788875b09f6c8530e782fdde7c4c011dcd6f
zinebrkh3-boop <zinebrkh3@gmail.com>
2026-06-29 23:21:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// يمكنك إضافة الأكواد الخاصة بـ Lab 6 هنا
+console.log("Projet Équilibra chargé !");
+
+// مثال بسيط للتفاعل
+document.body.addEventListener('click', () => {
+    console.log("تم النقر على الصفحة");
+});
